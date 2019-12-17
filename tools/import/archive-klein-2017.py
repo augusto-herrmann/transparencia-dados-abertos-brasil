5cb58ae5ed4a89f04356e0eaa3e1bb9ccabb839c
Augusto Herrmann <augusto+github@herrmann.tech>
2019-12-17 10:51:17 -0300

    improve script documentation

--- tools/import/archive-klein-2017.py
+++ tools/import/archive-klein-2017.py
@@ -1,12 +1,29 @@
+# archive-klein-2017.py
+# 
+# This script converts data from the Klein-2017 dataset in the archived
+# format (used to be the main data in this repository) to the current format
+# where state and municipal transparency and open data portals are represented
+# in a single CSV resource.
+# 
+# Este script converte os dados do dataset Klein-2017, no formato arquivado
+# (anteriormente era o conjunto de dados principal deste repositório) para o
+# formato atual em que os portais de transparência e dados abertos dos estados
+# e municípios estão representados em um único recurso CSV.
+# 
+
 import os
 import pandas as pd
 from datapackage import Package
 
-OUTPUT_FILE = '../../data/valid/brazilian-transparency-and-open-data-portals.csv'
+INPUT_PATH = '../../data/archive'
+INPUT_MUNICIPAL = 'portais-municipais.csv'
+INPUT_STATE = 'portais-estaduais.csv'
+OUTPUT_PATH = '../../data/valid'
+OUTPUT_FILE = 'brazilian-transparency-and-open-data-portals.csv'
 
 # state portals
 df_state = (
-    pd.read_csv('../../data/archive/portais-estaduais.csv')
+    pd.read_csv(os.path.join(INPUT_PATH,INPUT_STATE))
     .drop('uf_nome', axis=1)
     .rename(columns={'uf': 'state_code', 'tipo': 'type'})
 )
@@ -17,7 +34,7 @@ df_state['branch'] = 'executive'
 # municipality portals
 
 df_municipality = (
-    pd.read_csv('../../data/archive/portais-municipais.csv')
+    pd.read_csv(os.path.join(INPUT_PATH,INPUT_MUNICIPAL))
     .drop('população', axis=1)
     .rename(columns={
         'uf': 'state_code',
@@ -32,7 +49,7 @@ df_municipality['branch'] = 'executive'
 
 # concatenate both sources
 
-dp = Package('../../data/valid/datapackage.json')
+dp = Package(os.path.join(OUTPUT_PATH,'datapackage.json'))
 fields = dp.get_resource('brazilian-transparency-and-open-data-portals').schema.fields
 columns=[field.name for field in fields]
 
@@ -45,9 +62,9 @@ df = pd.concat([
 # merge existing data, if present
 
 df.set_index('url', inplace=True)
-if os.path.exists(OUTPUT_FILE):
+if os.path.exists(os.path.join(OUTPUT_PATH,OUTPUT_FILE)):
     df_existing = pd.read_csv(
-        OUTPUT_FILE,
+        os.path.join(OUTPUT_PATH,OUTPUT_FILE),
     ).set_index('url')
     df_new = pd.concat([
         df_existing,
@@ -58,5 +75,5 @@ else:
 df.reset_index(inplace=True)
 df = df[columns]
 
-df.to_csv(OUTPUT_FILE, index=False)
+df.to_csv(os.path.join(OUTPUT_PATH,OUTPUT_FILE), index=False)
 
