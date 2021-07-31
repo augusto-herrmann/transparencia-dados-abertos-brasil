47b200f91c75b7d55cec99bbcafa7508d9ad82cb
Augusto Herrmann <augusto+github@herrmann.tech>
2021-07-30 23:31:40 -0300

    fixes #20: add new query for cities in the state of Espírito Santo

--- tools/import/dbpedia/01-dbpedia-municipality-uris.py
+++ tools/import/dbpedia/01-dbpedia-municipality-uris.py
@@ -20,6 +20,7 @@ remove_parenthesis = re.compile(r'[^(,]+')
 
 DBPEDIA_PT_SPARQL = 'dbpedia-pt.sparql'
 DBPEDIA_SPARQL = 'dbpedia.sparql'
+ESPIRITO_SANTO_DBPEDIA_SPARQL = 'espirito-santo.dbpedia.sparql'
 DBPEDIA_PT_URL = 'http://pt.dbpedia.org/sparql?default-graph-uri=&{}&should-sponge=&format=text%2Fcsv&timeout=0&debug=on'
 DBPEDIA_URL = 'https://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&{}&format=text%2Fcsv&timeout=30000&signal_void=on&signal_unconnected=on'
 
@@ -54,19 +55,26 @@ def update_from_dbpedia(
     sparql_query_as_csv = sparql_query_url.format(sparql_query)
 
     # read data frame from Portuguese DBPedia
-    dbp_pt = pd.read_csv(sparql_query_as_csv)
+    dbp = pd.read_csv(sparql_query_as_csv)
 
     # remove parenthesis in city names
-    dbp_pt['name'] = dbp_pt.name.apply(
-        lambda s: remove_parenthesis.match(s).group().strip()
+    dbp['name'] = dbp['name'].apply(
+        lambda s: remove_parenthesis.match(s).group().strip()\
+            if isinstance(s, str) else s
+    )
+
+    # remove parenthesis in state names
+    dbp['state'] = dbp['state'].apply(
+        lambda s: remove_parenthesis.match(s).group().strip() \
+            if isinstance(s, str) else s
     )
 
     # get WikiData URIs for later
     wikidata = (
-        dbp_pt
+        dbp
         .loc[:, ['name', 'state', 'wikidata']]
-        .loc[dbp_pt.state.notna()]
-        .loc[dbp_pt.wikidata.notna()]
+        .loc[dbp.state.notna()]
+        .loc[dbp.wikidata.notna()]
         .drop_duplicates()
         .copy()
     )
@@ -91,7 +99,7 @@ def update_from_dbpedia(
     )
 
     # handle the different types of URIs – main DBPedia or pt DBPedia
-    dbp_pt['URI_type'] = dbp_pt.city.apply(
+    dbp['URI_type'] = dbp.city.apply(
         lambda s: 'dbpedia' \
             if s.startswith('http://dbpedia.org/') \
             else 'dbpedia_pt' \
@@ -100,20 +108,20 @@ def update_from_dbpedia(
     )
 
     # format the dataframe like the municipality table
-    dbp_pt = dbp_pt.merge(uf)
-    dbp_pt.drop('state', axis=1, inplace=True)
-    dbp_pt.rename(columns={'abbr': 'uf'}, inplace=True)
-    dbp_pt.rename(columns={'city': 'URI'}, inplace=True)
-    dbp_pt = dbp_pt.loc[:, ['name', 'uf', 'URI', 'URI_type']] # discard all other columns
-    dbp_pt.sort_values(by=['uf', 'name', 'URI'], inplace=True)
-    dbp_pt.drop_duplicates(subset=['name', 'uf', 'URI_type'], keep='first', inplace=True)
+    dbp = dbp.merge(uf)
+    dbp.drop('state', axis=1, inplace=True)
+    dbp.rename(columns={'abbr': 'uf'}, inplace=True)
+    dbp.rename(columns={'city': 'URI'}, inplace=True)
+    dbp = dbp.loc[:, ['name', 'uf', 'URI', 'URI_type']] # discard all other columns
+    dbp.sort_values(by=['uf', 'name', 'URI'], inplace=True)
+    dbp.drop_duplicates(subset=['name', 'uf', 'URI_type'], keep='first', inplace=True)
 
     # create dbpedia and dbpedia_pt columns depending on the value of URI
-    dbp_pt = (
-        dbp_pt
+    dbp = (
+        dbp
         .merge(
             (
-                dbp_pt
+                dbp
                 .pivot(index=['name', 'uf'], columns='URI_type', values='URI')
                 .reindex()
             ), on=['name', 'uf'], how='left'
@@ -126,36 +134,36 @@ def update_from_dbpedia(
     mun = package.get_resource('municipality').to_pandas()
 
     # just add the municipality codes to the dataframe
-    dbp_pt = dbp_pt.merge( 
+    dbp = dbp.merge( 
         mun.loc[:, ['code', 'name', 'uf']], # use just those columns for merge
         on=['name', 'uf'], # keys for the join operation
-        how='right', # keep the keys from mun dataframe even if not found on dbp_pt
+        how='right', # keep the keys from mun dataframe even if not found on dbp
     ).reindex(columns=['code', 'name', 'uf', 'dbpedia', 'dbpedia_pt'])
 
-    # add wikidata column to dbp_pt again
-    dbp_pt = (
-        dbp_pt
+    # add wikidata column to dbp again
+    dbp = (
+        dbp
         .merge(wikidata, on=['name', 'uf'], how='left')
     )
 
     # TODO: figure out what to do with extra/duplicated Wikidata URIs
-    # dbp_pt[dbp_pt.duplicated(subset=['name', 'uf'], keep=False)].to_csv('duplicated.csv', index=False)
+    # dbp[dbp.duplicated(subset=['name', 'uf'], keep=False)].to_csv('duplicated.csv', index=False)
 
     # if dataframe has more than one wikidata entry, consider only
     # the first one
-    dbp_pt.drop_duplicates(subset=['name', 'uf'], keep='first', inplace=True)
+    dbp.drop_duplicates(subset=['name', 'uf'], keep='first', inplace=True)
 
     # sort both dataframes to align them
-    assert len(dbp_pt) == len(mun) # must be the same size
-    dbp_pt.sort_values(by='code', inplace=True)
+    assert len(dbp) == len(mun) # must be the same size
+    dbp.sort_values(by='code', inplace=True)
     mun.sort_values(by='code', inplace=True)
-    dbp_pt.set_index(dbp_pt.code, inplace=True) # make the index be the code
+    dbp.set_index(dbp.code, inplace=True) # make the index be the code
     mun.set_index(mun.code, inplace=True) # make the index be the code
 
     # update the URIs, if present. Otherwise, preserve the old ones
-    mun['dbpedia'] = update_column(mun, dbp_pt, 'dbpedia')
-    mun['dbpedia_pt'] = update_column(mun, dbp_pt, 'dbpedia_pt')
-    mun['wikidata'] = update_column(mun, dbp_pt, 'wikidata')
+    mun['dbpedia'] = update_column(mun, dbp, 'dbpedia')
+    mun['dbpedia_pt'] = update_column(mun, dbp, 'dbpedia_pt')
+    mun['wikidata'] = update_column(mun, dbp, 'wikidata')
 
     # write back the csv
     mun.to_csv(output_file, index=False)
@@ -173,5 +181,12 @@ if __name__ == '__main__':
         os.path.join(OUTPUT_FOLDER, OUTPUT_FILE),
         os.path.join(GEO_FOLDER, GEO_FILE),
         DBPEDIA_SPARQL,
+        DBPEDIA_URL
+    )
+    # state of Espírito Santo from English DBPedia
+    update_from_dbpedia(
+        os.path.join(OUTPUT_FOLDER, OUTPUT_FILE),
+        os.path.join(GEO_FOLDER, GEO_FILE),
+        ESPIRITO_SANTO_DBPEDIA_SPARQL,
         DBPEDIA_URL
     )
