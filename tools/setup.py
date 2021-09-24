5883a6bc5f4ef03fb944868b1995f146f7df289e
Augusto Herrmann <augusto+github@herrmann.tech>
2021-09-24 20:49:19 -0300

    add basic module information (issue #25)

---
+++ tools/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='TransparencyOpenDataBR',
+    version='0.0.1',
+    author='Augusto Herrmann',
+    author_email='augusto+github@herrmann.tech',
+    packages=['.'],
+    license='LICENSE',
+    description=("A survey of Brazilian states' and municipalities' "
+        "transparency and open data portals, as well as institutional "
+        "websites, obtained from several public data sources.")
+)
