88cff19369ddcf57a884985d6a9d11d0b642fa28
Augusto Herrmann <augusto+github@herrmann.tech>
2022-08-21 14:01:54 -0300

    Make manually_verify_links module friendly

--- tools/validation/manually_verify_links.py
+++ tools/validation/manually_verify_links.py
@@ -1,22 +1,21 @@
-# manually_verify_links.py
-#
-# This script opens candidate URLs for municipalities websites in the
-# web browser and asks the user to check whether or not they seem to be the
-# official city hall or city council portals.
-# 
-# Usage:
-#   python manually_verify_links.py
-#
-# Este script abre no navegador as URLs candidatas a sites dos municípios e
-# pede ao utilizador que verifique se elas parcem ser os portais das
-# prefeituras e câmaras municipais.
-#
+"""
+This script opens candidate URLs for municipalities websites in the
+web browser and asks the user to check whether or not they seem to be the
+official city hall or city council portals.
+
+Usage:
+  python manually_verify_links.py
+
+Este script abre no navegador as URLs candidatas a sites dos municípios e
+pede ao utilizador que verifique se elas parecem ser os portais das
+prefeituras e câmaras municipais.
+"""
 
 import os
 import argparse
 from datetime import datetime
 import random
-import warnings
+import logging
 import webbrowser
 
 import pandas as pd
@@ -25,82 +24,118 @@ from bs4 import BeautifulSoup
 from unidecode import unidecode
 from frictionless import Package
 
-USER_AGENT = 'transparencia-dados-abertos-brasil/0.0.1'
+USER_AGENT = 'transparencia-dados-abertos-brasil/0.0.2'
+TIMEOUT = 20
 INPUT_FOLDER = '../../data/unverified'
 INPUT_FILE = 'municipality-website-candidate-links.csv'
 OUTPUT_FOLDER = '../../data/valid'
-OUTPUT_FILE = 'brazilian-municipality-and-state-websites.csv'
+MAX_QUANTITY = 0
 
-# Command line interface
-parser = argparse.ArgumentParser(
-    description='''Opens in a web browser candidate URLs for municipalities
-    websites and asks the user to checks if they seem to be the city hall or
-    city council portals.
-    
-Abre no navegador as URLs candidatas a sites dos municípios e  pede ao
-    utilizador que verifique se elas parcem ser os portais das prefeituras e
-    câmaras municipais.
-    '''
+def parse_cli() -> dict:
+    """Parses the command line interface.
+
+    Returns:
+        dict: A dict containing the values for input_folder, input_file,
+            data_package_path, max_quantity, max_simultaneous
+    """
+    parser = argparse.ArgumentParser(
+        description='''Opens in a web browser candidate URLs for municipalities
+        websites and asks the user to checks if they seem to be the city hall or
+        city council portals.
+        
+    Abre no navegador as URLs candidatas a sites dos municípios e  pede ao
+        utilizador que verifique se elas parecem ser os portais das prefeituras e
+        câmaras municipais.
+        '''
+        )
+
+    parser.add_argument('input',
+        help='input file in CSV format / arquivo em formato CSV',
+        default='',
+        nargs='?',
+        )
+    parser.add_argument('output',
+        help=('output folder for the CSV '
+            '(must have a datapackage.json with a schema) / '
+            'pasta de saída para o CSV '
+            '(precisa ter um datapackage.json com um esquema)'),
+        default='',
+        nargs='?',
     )
+    parser.add_argument('-q', '--quantity',
+        metavar='int', type=int,
+        help='maximum quantity of cities to process / quantidade máxima a processar',
+        default=0,
+        )
+    params = {}
+    args = parser.parse_args()
+    if args.input:
+        params['input_folder'] = os.path.dirname(args.input)
+        params['input_file'] = os.path.basename(args.input)
+    else: # use default values
+        params['input_folder'] = INPUT_FOLDER
+        params['input_file'] = INPUT_FILE
+    if args.output:
+        if not os.path.exists(args.output):
+            raise FileNotFoundError(f'Folder not found: {args.output}')
+        output_folder = args.output
+    else: # use default value
+        output_folder = OUTPUT_FOLDER
+    params['data_package_path'] = os.path.join(output_folder, 'datapackage.json')
+    if not os.path.exists(params['data_package_path']):
+        raise FileNotFoundError(
+            f'datapackage.json not found in folder: {args.output}')
+    if args.quantity:
+        params['max_quantity'] = args.quantity
+    else: # use default value
+        params['max_quantity'] = MAX_QUANTITY
 
-parser.add_argument('input',
-                    help='input file in CSV format / arquivo em formato CSV',
-                    default='',
-                    nargs='?',
-                    )
-parser.add_argument('output',
-                    help='input file in CSV format / arquivo em formato CSV',
-                    default='',
-                    nargs='?',
-                    )
-parser.add_argument('-q', '--quantity',
-                    metavar='int', type=int,
-                    help='maximum quantity of cities to process / quantidade máxima a processar',
-                    default=0,
-                    )
-args = parser.parse_args()
-if args.input:
-    INPUT_FOLDER = os.path.dirname(args.input)
-    INPUT_FILE = os.path.basename(args.input)
-if args.output:
-    OUTPUT_FOLDER = os.path.dirname(args.output)
-    OUTPUT_FILE = os.path.basename(args.output)
-if args.quantity:
-    MAX_QUANTITY = args.quantity
-else:
-    MAX_QUANTITY = None
+    return params
 
-candidates = pd.read_csv(os.path.join(INPUT_FOLDER, INPUT_FILE))
-print (f'Found {len(candidates)} websites in {os.path.join(INPUT_FOLDER, INPUT_FILE)}.')
-print (f'Encontrados {len(candidates)} sites em {os.path.join(INPUT_FOLDER, INPUT_FILE)}.')
-codes = candidates.code.unique()
-random.shuffle(codes) # randomize sequence
-if MAX_QUANTITY:
-    codes = codes[:MAX_QUANTITY] # take a subsample for shorter session
-else:
-    MAX_QUANTITY = len(codes)
-goodlinks = pd.DataFrame(columns=candidates.columns)
+def choose(text: str, options: str) -> str:
+    """Asks the user to choose one of the defined options.
 
-def choose(text, options):
-    'Asks the user to choose one of the defined options.'
+    Args:
+        text (str): The text prompt to be presented to the user.
+        options (str): A string containing one character for each option
+            the user can choose.
+
+    Returns:
+        str: The key containing one character representing what the user
+            has chosen.
+    """
     while True:
         key = input(text).lower()
         if key in options.lower():
             break
     return key
 
-def healthy_link(link):
-    'Check whether the link is healthy or not.'
+def healthy_link(link: str) -> requests.Response:
+    """Check whether or not the link is healthy.
+
+    Args:
+        link (str): The url of the link to be verified.
+
+    Returns:
+        requests.Response: The Response object in case the link
+            is healthy, None otherwise.
+    """
     try:
-        r = requests.get(link, headers={'user-agent': USER_AGENT})
+        response = requests.get(
+            link,
+            headers={'user-agent': USER_AGENT},
+            timeout=TIMEOUT
+            )
     except (
         requests.exceptions.ConnectionError,
-        requests.exceptions.InvalidURL
+        requests.exceptions.InvalidURL,
+        requests.exceptions.TooManyRedirects,
+        requests.exceptions.ReadTimeout
     ):
-        r = None
-    if r and r.status_code == 200:
-        return r
-    return False
+        return None
+    if response and response.status_code == 200:
+        return response
+    return None
 
 def title_and_type(r, candidates):
     'Try to infer the type of site this is.'
@@ -124,7 +159,8 @@ def title_and_type(r, candidates):
     elif 'camara de' in title.lower():
         link_type = 'camara'
     else:
-        warnings.warn(f'Unable to determine site type from title: “{title}”.')
+        logging.warning(
+            'Unable to determine site type from title: “%s”.', title)
         link_type = None
     return title, link_type
 
@@ -183,42 +219,94 @@ def verify_city_links(candidates, code):
             print('  Error opening URL.')
     return signal, verified_links
 
-results = []
-print (f'Verifying candidate URLs for {min(len(codes),MAX_QUANTITY)} cities...')
-for code in codes[:MAX_QUANTITY]:
-    signal, links_to_add = verify_city_links(candidates[candidates.code == code], code)
-    if signal == 'q':
-        print('Quitting...')
-        break
+def manual_verify(input_folder: str, input_file: str, data_package_path: str,
+        max_quantity: int) -> pd.DataFrame:
+    """Manually verifies links by opening each one of them on the browser
+    for the user to check. Then asks the user to classify the link type.
+
+    A Python function that does the same job as the script that is run
+    from the command line.
+
+    Args:
+        input_folder (str): The folder containing the input table.
+        input_file (str): Name of the file containing the input table in
+            csv format.
+        data_package_path (str): Path to the datapackage.json file.
+        max_quantity (int): Maximum quantity of links to check.
+
+    Returns:
+        pd.DataFrame: Pandas dataframe containing the verified links.
+    """
+    candidates = pd.read_csv(os.path.join(input_folder, input_file))
+    logging.info(
+        'Found %d websites in %s.',
+        len(candidates),
+        os.path.join(input_folder, input_file))
+    logging.info(
+        'Encontrados %d websites em %s.',
+        len(candidates),
+        os.path.join(input_folder, input_file))
+    codes = candidates.code.unique()
+    random.shuffle(codes) # randomize sequence
+    if max_quantity:
+        codes = codes[:max_quantity] # take a subsample for shorter session
     else:
+        max_quantity = len(codes)
+
+    results = []
+    print(f'Verifying candidate URLs for {max_quantity} cities...')
+    for code in codes:
+        signal, links_to_add = verify_city_links(candidates[candidates.code == code], code)
+        if signal == 'q':
+            print('Quitting...')
+            break
         results.extend(links_to_add)
 
-# read resource to be updated
-package = Package(os.path.join(OUTPUT_FOLDER, 'datapackage.json'))
-resource = package.get_resource('brazilian-municipality-and-state-websites')
-df = resource.to_pandas()
+    # read resource to be updated
+    package = Package(data_package_path)
+    resource = package.get_resource('brazilian-municipality-and-state-websites')
+    table = resource.to_pandas()
 
-print('Updating values...')
-for result in results:
-    # get existing data in file to be updated
-    existing_data = df.loc[
-            (df.municipality_code == result['municipality_code']) &
-            (df.branch == result['branch'])
-        ]
-    if len(existing_data) > 0:
-        row = existing_data.iloc[0].copy()
-        for key in ['sphere', 'branch', 'url', 'last-verified-manual']:
-            row[key] = result[key] # update the values
-        df = df.append(row, ignore_index=True)
-    else:
-        df = df.append(result, ignore_index=True)
+    print('Updating values...')
+    for result in results:
+        # get existing data in file to be updated
+        existing_data = table.loc[
+                (table.municipality_code == result['municipality_code']) &
+                (table.branch == result['branch'])
+            ]
+        if len(existing_data) > 0:
+            row = existing_data.iloc[0].copy()
+            for key in ['sphere', 'branch', 'url', 'last-verified-manual']:
+                row[key] = result[key] # update the values
+            table = table.append(row, ignore_index=True)
+        else:
+            table = table.append(result, ignore_index=True)
 
-output = r.source # filename of csv to write
-print(f'Recording {output}...')
-# remove duplicate entries,
-# take into account only url column,
-# keep last entry to preserve the last-verified-auto timestamp
-df.drop_duplicates(subset='url', keep='last', inplace=True)
-df.sort_values(by=['state_code', 'municipality'], inplace=True)
-# store the results
-df.to_csv(output, index=False, date_format='%Y-%m-%dT%H:%M:%SZ')
+    output = r.source # filename of csv to write
+    print(f'Recording {output}...')
+    # remove duplicate entries,
+    # take into account only url column,
+    # keep last entry to preserve the last-verified-auto timestamp
+    table.drop_duplicates(subset='url', keep='last', inplace=True)
+    table.sort_values(by=['state_code', 'municipality'], inplace=True)
+    return table
+
+def store_csv(table: pd.DataFrame, data_package_path: str):
+    """Stores the csv file in the output folder.
+
+    Args:
+        table (pd.DataFrame): The dataframe containing the data.
+        data_package_path (str): Path to the datapackage.json file.
+    """
+    package = Package(data_package_path)
+    resource = package.get_resource('brazilian-municipality-and-state-websites')
+    output = resource.path # filename of csv to write
+    logging.info('Recording %s...', output)
+    # store the file
+    table.to_csv(output, index=False, date_format='%Y-%m-%dT%H:%M:%SZ')
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    options = parse_cli()
+    table = manual_verify(**options)
+    store_csv(table, options['data_package_path'])
