d3acbe1ae9fe0231ebd632de3e119a7270005827
Augusto Herrmann <augusto+github@herrmann.tech>
2022-08-21 20:34:56 -0300

    Fix and update ibge import script
    fixes #51

--- tools/harvest/ibge/ibge_municipalities.py
+++ tools/harvest/ibge/ibge_municipalities.py
@@ -1,18 +1,16 @@
-# ibge_municipalities.py
-# 
-# This script downloads and stores auxiliary data from IBGE about
-# municipalities. This will be useful later for disambiguation and finding out
-# the municipality codes.
-#
-# Usage:
-#   python ibge_municipalities.py
-#
-# Este script faz o download e armazena dados auxiliares do IBGE sobre os
-# municípios. Isso será útil mais tarde para desambiguação e para descobrir
-# os códigos de municípios.
-#
+"""This script downloads and stores auxiliary data from IBGE about Brazilian
+municipalities. This will be useful later for disambiguation and finding out
+the municipality codes.
 
-import os, io
+Usage:
+  python ibge_municipalities.py
+
+Este script faz o download e armazena dados auxiliares do IBGE sobre os
+municípios. Isso será útil mais tarde para desambiguação e para descobrir
+os códigos de municípios.
+"""
+
+import os, io, pathlib
 import urllib
 import requests
 import ftplib
@@ -23,8 +21,8 @@ from tqdm import tqdm
 import pandas as pd
 from frictionless import Package
 
-TEMPORARY_FOLDER = 'download-cache'
-DOWNLOAD_URL = 'ftp://geoftp.ibge.gov.br/organizacao_do_territorio/estrutura_territorial/divisao_territorial/2018/DTB_2018.zip'
+TEMPORARY_FOLDER = '../../../data/download-cache'
+DOWNLOAD_URL = 'https://geoftp.ibge.gov.br/organizacao_do_territorio/estrutura_territorial/divisao_territorial/2021/DTB_2021.zip'
 OUTPUT_FOLDER = '../../../data/auxiliary/geographic'
 OUTPUT_FILE = 'municipality.csv'
 
@@ -34,10 +32,10 @@ def download_file(folder, url):
     'Download a (large) file using a progress bar.'
     local_filename = os.path.join(folder,url.split('/')[-1])
     # NOTE the stream=True parameter below
-    with requests.get(url, stream=True) as request:
-        request.raise_for_status()
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()
         with open(local_filename, 'wb') as file:
-            for chunk in tqdm(r.iter_content(chunk_size=8192)):
+            for chunk in tqdm(response.iter_content(chunk_size=8192)):
                 if chunk: # filter out keep-alive new chunks
                     file.write(chunk)
                     # f.flush()
@@ -76,11 +74,11 @@ def fetch_ibge_spreadsheet(tmp_path: str,
 
     if not os.path.exists(tmp_path):
         logging.info('Temporary folder does not yet exist. Creating "%s"...', tmp_path)
-        os.mkdir(tmp_path)
+        pathlib.Path(tmp_path).mkdir(parents=True, exist_ok=True)
 
     if not os.path.exists(os.path.join(tmp_path, file_name)):
         logging.info('IBGE file does not yet exist. Downloading from "%s"...', url)
-        download_ftp_file(tmp_path, url)
+        download_file(tmp_path, url)
 
     # unpack the zipped file
     with ZipFile(os.path.join(tmp_path, file_name)) as pacote:
@@ -131,33 +129,52 @@ def remove_and_rename_columns(table: pd.DataFrame) -> pd.DataFrame:
     Returns:
         pd.DataFrame: The dataframe adapted to our schema.
     """
-    table.drop(
-        [
-            'UF',
-            'Nome_UF',
-            'Mesorregião Geográfica',
-            'Nome_Mesorregião',
-            'Microrregião Geográfica',
-            'Nome_Microrregião',
-            'Município'
-        ],
-        axis=1,
-        inplace=True
-    )
-
-    table.rename(
-        columns={
+    table =(
+        table
+        .rename(columns={
             'Código Município Completo': 'code',
             'Nome_Município': 'name',
             'Sigla_UF':'uf'
-        },
-        inplace=True
+        })
+        .loc[:,['code', 'name', 'uf']]
+        .sort_values(by='code')
     )
 
     return table
+
+def merge_existing(table: pd.DataFrame, data_package_path: str) -> pd.DataFrame:
+    """Merges with existing data.
+
+    Args:
+        table (pd.DataFrame): A Pandas dataframe with the new data.
+        data_package_path (str): Path to the data package with exsiting
+            data.
+
+    Returns:
+        pd.DataFrame: The pandas dataframe with the merged data.
+    """
+    # checks if the table already exists. If not, return just the new data
+    data_package_file_name = os.path.join(data_package_path,
+        'datapackage.json')
+    if not os.path.exists(data_package_file_name):
+        print(data_package_file_name)
+        return table
+    package = Package(data_package_file_name)
+    resource = package.get_resource('municipality')
+    resource_path = os.path.join(data_package_path, resource.path)
+    if not os.path.exists(resource_path):
+        print(resource_path)
+        return table
+    old_table = resource.to_pandas()
+    # update rows that might have changed
+    old_table.set_index('code').update(table.set_index('code'))
+    # add new rows that might have been added
+    new_values = table[~table.code.isin(old_table.code.unique())]
+    return pd.concat([old_table, new_values])
 
 if __name__ == '__main__':
     table = fetch_ibge_spreadsheet(TEMPORARY_FOLDER, DOWNLOAD_URL)
     table = add_state_codes(table)
     table = remove_and_rename_columns(table)
+    table = merge_existing(table, OUTPUT_FOLDER)
     table.to_csv(os.path.join(OUTPUT_FOLDER, OUTPUT_FILE), index=False)
